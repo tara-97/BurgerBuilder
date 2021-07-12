885eeed4ebe03c32aa56179187dc19e11522fc47
tara97 <taraprasad9090@gmail.com>
2021-07-12 23:46:15 +0530

    Adding Navigation to the app

--- BurgerApp/src/components/Layout/Layout.js
+++ BurgerApp/src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Auxi'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
 const layout = (props) => (
     <Aux>
-        <div>Toolbar,SideDrawer,Backdrop</div>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
